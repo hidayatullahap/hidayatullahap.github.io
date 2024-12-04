0604d74c6b0b85c82df561b699c06ddabaf9d52b
Hidayatullah Agung <hidayatullahap@gmail.com>
2024-12-04 21:18:28 +0700

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        colors: {
+            'darkest': '#1A1A19',
+            'dark': '#31511E',
+            'light': '#859F3D',
+            'lightest': '#F6FCDF',
+        },
+    },
+}
