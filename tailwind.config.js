3356e18e0deded49cc3ec1d81d0eb373fc3608d6
Hidayatullah Agung <hidayatullahap@gmail.com>
2024-12-04 21:39:32 +0700

    add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     content: ["./src/**/*.{html,js}", "./index.html"],
     theme: {
         colors: {
-            'darkest': '#1A1A19',
+            'darkest': '#191919',
             'dark': '#31511E',
             'light': '#859F3D',
             'lightest': '#F6FCDF',
